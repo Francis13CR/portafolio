9ca1ab8b5584914e6804f90e2b1ccc65817fa5ab
francismelendez134@gmail.com <francismelendez134@gmail.com>
2023-06-22 23:50:47 -0600

    mas avances en el proyexto

--- wp-content/themes/hestia-child/assets/js/scripts.js
+++ wp-content/themes/hestia-child/assets/js/scripts.js
@@ -1,3 +1,26 @@
 jQuery(function($){
-//    $('.nav-hestia-child').slicknav();
+    // if (window.location.href.indexOf('/category/') > -1) {
+    //     $('.footer').addClass('ocultar');
+    // }
+    $('.moretag').text(" Ver más...")
+    $('.wp-block-columns').css('opacity', 0); // Ocultar inicialmente los elementos
+  
+    $('.wp-block-columns').waypoint(function(direction) {
+      if (direction === 'down') {
+        $(this.element).animate({ opacity: 1 }, 1000); // Fade-in
+      } else if (direction === 'up') {
+        $(this.element).animate({ opacity: 0 }, 1000, function() {
+          $(this).css('opacity', 0); // Restablecer la opacidad a 0 al completar el fade-out
+        }); // Fade-out
+      }
+    }, {
+      offset: '98%',
+      handler: function(direction) {
+        if (direction === 'up') {
+          $(this.element).css('opacity', 0); // Ocultar los elementos cuando se vuelven invisibles
+        }
+      }
+    });
+  
+   
 })
--- wp-content/themes/hestia-child/functions.php
+++ wp-content/themes/hestia-child/functions.php
@@ -19,11 +19,21 @@ function home(){
 }
 add_action( 'hestia_after_header_hook', 'home' );
 
+function ocultar_footer_categoria() {
+    if (is_category('progra')) {
+        remove_action('wp_footer', 'hestia_do_footer');
+    }
+}
+add_action('init', 'ocultar_footer_categoria');
+
+
 //Para agregar librerias
 function my_libraries(){
     wp_enqueue_style('slicknav_styles','https://cdnjs.cloudflare.com/ajax/libs/SlickNav/1.0.0/slicknav.min.css',[],'1.0.0',true);
     wp_enqueue_script('slicknav_script','https://cdnjs.cloudflare.com/ajax/libs/SlickNav/1.0.0/jquery.slicknav.js',[],'1.0.0',true);
-    wp_enqueue_script('scripts',get_stylesheet_directory_uri().'/assets/js/scripts.js',['jquery', 'slicknav_script'],'1.0.0',true);
+    wp_enqueue_script('waypoints_script', 'https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js', ['jquery'], '4.0.1', true);
+    wp_enqueue_script('scripts',get_stylesheet_directory_uri().'/assets/js/scripts.js',['jquery', 'slicknav_script', 'waypoints_script'],'1.0.0',true);
+
 }
 add_action('wp_enqueue_scripts','my_libraries');
 
